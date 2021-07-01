2a2dce6779f2227a082255262f5177972a509497
Dylan Southard <southard.dylan@gmail.com>
2021-07-01 20:20:56 +0700

    Initial Commit

---
+++ PlayerList/PlayerListApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerListApp.swift
+//  PlayerList
+//
+//  Created by Dylan Southard on 1/7/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct PlayerListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
